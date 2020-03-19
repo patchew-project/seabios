080dc880ab84a0500743570caa2feaac00cf735d
Gerd Hoffmann <kraxel@redhat.com>
2020-03-19 08:39:34 +0100

    virtio-mmio: device probing and initialization.
    Add virtio_mmio_setup() to probe virtio mmio devices.  Add
    vp_init_mmio() to initialize device struct.  Because virtio-pci and
    virtio-mmio are quite simliar we reuse the infrastructure we already
    have for virtio-pci and just setup struct vp_cap for virtio-mmio.
    
    Signed-off-by: Gerd Hoffmann <kraxel@redhat.com>
    Message-Id: <20200319073937.5561-4-kraxel@redhat.com>

--- src/hw/virtio-mmio.c
+++ src/hw/virtio-mmio.c
@@ -1,8 +1,11 @@
 #include "config.h" // CONFIG_DEBUG_LEVEL
 #include "malloc.h" // free
 #include "output.h" // dprintf
+#include "stacks.h" // run_thread
+#include "string.h" // memset
 #include "virtio-pci.h"
 #include "virtio-ring.h"
+#include "virtio-mmio.h"
 
 /* qemu microvm supports 8 virtio-mmio devices */
 static u64 devs[8];
@@ -27,4 +30,50 @@ void virtio_mmio_register(u64 mmio)
         }
     }
     dprintf(1, "virtio-mmio: device list full\n");
+}
+
+void virtio_mmio_setup(void)
+{
+    u32 magic, version, devid;
+    void *mmio;
+    int i;
+
+    for (i = 0; i < ARRAY_SIZE(devs); i++) {
+        if (devs[i] == 0)
+            return;
+        mmio = (void*)(u32)(devs[i]);
+        magic = readl(mmio);
+        if (magic != 0x74726976)
+            continue;
+        version = readl(mmio+4);
+        if (version != 1 /* legacy */ &&
+            version != 2 /* 1.0 */)
+            continue;
+        devid = readl(mmio+8);
+        dprintf(1, "virtio-mmio: %llx: device id %x%s\n",
+                devs[i], devid, version == 1 ? " (legacy)" : "");
+        switch (devid) {
+        case 2: /* blk */
+            /* TODO */
+            break;
+        case 8: /* scsi */
+            /* TODO */
+            break;
+        default:
+            break;
+        }
+    }
+}
+
+void vp_init_mmio(struct vp_device *vp, void *mmio)
+{
+    memset(vp, 0, sizeof(*vp));
+    vp->use_mmio = 1;
+    vp->common.mode = VP_ACCESS_MMIO;
+    vp->common.memaddr = mmio;
+    vp->device.mode = VP_ACCESS_MMIO;
+    vp->device.memaddr = mmio + 0x100;
+    vp_reset(vp);
+    vp_set_status(vp, VIRTIO_CONFIG_S_ACKNOWLEDGE |
+                  VIRTIO_CONFIG_S_DRIVER);
 }
