patchew-project/seabios

commit 6a3b59ab9c7dc00331c21346052dfa6a0df45aa3

pci: add mmconfig support
Add support for pci config space access via mmconfig bar.  Enable for
qemu q35 chipset.  Main advantage is that we need only one instead of
two io operations per config space access, which translates to one
instead of two vmexits for virtualization.

Signed-off-by: Gerd Hoffmann <kraxel@redhat.com>
Message-id: 20200323145911.22319-3-kraxel@redhat.com

src/hw/pci.c
@@ -14,6 +14,13 @@
 #define PORT_PCI_CMD           0x0cf8
 #define PORT_PCI_DATA          0x0cfc
 
+static u32 mmconfig;
+
+static void *mmconfig_addr(u16 bdf, u32 addr)
+{
+    return (void*)(mmconfig + ((u32)bdf << 12) + addr);
+}
+
 static u32 ioconfig_cmd(u16 bdf, u32 addr)
 {
     return 0x80000000 | (bdf << 8) | (addr & 0xfc);
@@ -21,38 +28,62 @@ static u32 ioconfig_cmd(u16 bdf, u32 addr)
 
 void pci_config_writel(u16 bdf, u32 addr, u32 val)
 {
-    outl(ioconfig_cmd(bdf, addr), PORT_PCI_CMD);
-    outl(val, PORT_PCI_DATA);
+    if (MODESEGMENT && mmconfig) {
+        writel(mmconfig_addr(bdf, addr), val);
+    } else {
+        outl(ioconfig_cmd(bdf, addr), PORT_PCI_CMD);
+        outl(val, PORT_PCI_DATA);
+    }
 }
 
 void pci_config_writew(u16 bdf, u32 addr, u16 val)
 {
-    outl(ioconfig_cmd(bdf, addr), PORT_PCI_CMD);
-    outw(val, PORT_PCI_DATA + (addr & 2));
+    if (MODESEGMENT && mmconfig) {
+        writew(mmconfig_addr(bdf, addr), val);
+    } else {
+        outl(ioconfig_cmd(bdf, addr), PORT_PCI_CMD);
+        outw(val, PORT_PCI_DATA + (addr & 2));
+    }
 }
 
 void pci_config_writeb(u16 bdf, u32 addr, u8 val)
 {
-    outl(ioconfig_cmd(bdf, addr), PORT_PCI_CMD);
-    outb(val, PORT_PCI_DATA + (addr & 3));
+    if (MODESEGMENT && mmconfig) {
+        writeb(mmconfig_addr(bdf, addr), val);
+    } else {
+        outl(ioconfig_cmd(bdf, addr), PORT_PCI_CMD);
+        outb(val, PORT_PCI_DATA + (addr & 3));
+    }
 }
 
 u32 pci_config_readl(u16 bdf, u32 addr)
 {
-    outl(ioconfig_cmd(bdf, addr), PORT_PCI_CMD);
-    return inl(PORT_PCI_DATA);
+    if (MODESEGMENT && mmconfig) {
+        return readl(mmconfig_addr(bdf, addr));
+    } else {
+        outl(ioconfig_cmd(bdf, addr), PORT_PCI_CMD);
+        return inl(PORT_PCI_DATA);
+    }
 }
 
 u16 pci_config_readw(u16 bdf, u32 addr)
 {
-    outl(ioconfig_cmd(bdf, addr), PORT_PCI_CMD);
-    return inw(PORT_PCI_DATA + (addr & 2));
+    if (MODESEGMENT && mmconfig) {
+        return readw(mmconfig_addr(bdf, addr));
+    } else {
+        outl(ioconfig_cmd(bdf, addr), PORT_PCI_CMD);
+        return inw(PORT_PCI_DATA + (addr & 2));
+    }
 }
 
 u8 pci_config_readb(u16 bdf, u32 addr)
 {
-    outl(ioconfig_cmd(bdf, addr), PORT_PCI_CMD);
-    return inb(PORT_PCI_DATA + (addr & 3));
+    if (MODESEGMENT && mmconfig) {
+        return readb(mmconfig_addr(bdf, addr));
+    } else {
+        outl(ioconfig_cmd(bdf, addr), PORT_PCI_CMD);
+        return inb(PORT_PCI_DATA + (addr & 3));
+    }
 }
 
 void
@@ -61,6 +92,15 @@ pci_config_maskw(u16 bdf, u32 addr, u16 off, u16 on)
     u16 val = pci_config_readw(bdf, addr);
     val = (val & ~off) | on;
     pci_config_writew(bdf, addr, val);
+}
+
+void
+pci_enable_mmconfig(u64 addr, const char *name)
+{
+    if (addr >= 0x100000000ll)
+        return;
+    dprintf(1, "PCIe: using %s mmconfig at 0x%llx\n", name, addr);
+    mmconfig = addr;
 }
 
 u8 pci_find_capability(u16 bdf, u8 cap_id, u8 cap)

src/fw/pciinit.c
@@ -480,6 +480,7 @@ static void mch_mmconfig_setup(u16 bdf)
     pci_config_writel(bdf, Q35_HOST_BRIDGE_PCIEXBAR, 0);
     pci_config_writel(bdf, Q35_HOST_BRIDGE_PCIEXBAR + 4, upper);
     pci_config_writel(bdf, Q35_HOST_BRIDGE_PCIEXBAR, lower);
+    pci_enable_mmconfig(Q35_HOST_BRIDGE_PCIEXBAR_ADDR, "q35");
 }
 
 static void mch_mem_addr_setup(struct pci_device *dev, void *arg)

src/hw/pci.h
@@ -39,6 +39,7 @@ u32 pci_config_readl(u16 bdf, u32 addr);
 u16 pci_config_readw(u16 bdf, u32 addr);
 u8 pci_config_readb(u16 bdf, u32 addr);
 void pci_config_maskw(u16 bdf, u32 addr, u16 off, u16 on);
+void pci_enable_mmconfig(u64 addr, const char *name);
 u8 pci_find_capability(u16 bdf, u8 cap_id, u8 cap);
 int pci_next(int bdf, int bus);
 int pci_probe_host(void);

commit 57c2f20e511a91b6b5d394678ecc5190534db046

pci: fix mmconfig support
The MODESEGMENT condition is backwards, with the effect that
mmconfig mode is not used to configure pci bars during POST.

Oops.  Fix it.

The only real mode pci config space access seems to come from the
ipxe option rom initialiation.  Which happens to work via mmconfig
because it runs in big real mode so this went unnoticed ...

Fixes: 6a3b59ab9c7d ("pci: add mmconfig support")
Signed-off-by: Gerd Hoffmann <kraxel@redhat.com>
Message-Id: <20200525090627.6393-1-kraxel@redhat.com>
Reviewed-by: Paul Menzel <pmenzel@molgen.mpg.de>

src/hw/pci.c
@@ -28,7 +28,7 @@ static u32 ioconfig_cmd(u16 bdf, u32 addr)
 
 void pci_config_writel(u16 bdf, u32 addr, u32 val)
 {
-    if (MODESEGMENT && mmconfig) {
+    if (!MODESEGMENT && mmconfig) {
         writel(mmconfig_addr(bdf, addr), val);
     } else {
         outl(ioconfig_cmd(bdf, addr), PORT_PCI_CMD);
@@ -38,7 +38,7 @@ void pci_config_writel(u16 bdf, u32 addr, u32 val)
 
 void pci_config_writew(u16 bdf, u32 addr, u16 val)
 {
-    if (MODESEGMENT && mmconfig) {
+    if (!MODESEGMENT && mmconfig) {
         writew(mmconfig_addr(bdf, addr), val);
     } else {
         outl(ioconfig_cmd(bdf, addr), PORT_PCI_CMD);
@@ -48,7 +48,7 @@ void pci_config_writew(u16 bdf, u32 addr, u16 val)
 
 void pci_config_writeb(u16 bdf, u32 addr, u8 val)
 {
-    if (MODESEGMENT && mmconfig) {
+    if (!MODESEGMENT && mmconfig) {
         writeb(mmconfig_addr(bdf, addr), val);
     } else {
         outl(ioconfig_cmd(bdf, addr), PORT_PCI_CMD);
@@ -58,7 +58,7 @@ void pci_config_writeb(u16 bdf, u32 addr, u8 val)
 
 u32 pci_config_readl(u16 bdf, u32 addr)
 {
-    if (MODESEGMENT && mmconfig) {
+    if (!MODESEGMENT && mmconfig) {
         return readl(mmconfig_addr(bdf, addr));
     } else {
         outl(ioconfig_cmd(bdf, addr), PORT_PCI_CMD);
@@ -68,7 +68,7 @@ u32 pci_config_readl(u16 bdf, u32 addr)
 
 u16 pci_config_readw(u16 bdf, u32 addr)
 {
-    if (MODESEGMENT && mmconfig) {
+    if (!MODESEGMENT && mmconfig) {
         return readw(mmconfig_addr(bdf, addr));
     } else {
         outl(ioconfig_cmd(bdf, addr), PORT_PCI_CMD);
@@ -78,7 +78,7 @@ u16 pci_config_readw(u16 bdf, u32 addr)
 
 u8 pci_config_readb(u16 bdf, u32 addr)
 {
-    if (MODESEGMENT && mmconfig) {
+    if (!MODESEGMENT && mmconfig) {
         return readb(mmconfig_addr(bdf, addr));
     } else {
         outl(ioconfig_cmd(bdf, addr), PORT_PCI_CMD);