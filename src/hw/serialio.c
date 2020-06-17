1ba945c7984f65a4f9ad441e50f8cd456572ffd5
Jason Andryuk <jandryuk@gmail.com>
2020-06-16 22:31:17 -0400

    Preserve Xen DebugOutputPort
    xen_preinit() runs early and changes DebugOutputPort.  qemu_preinit() runs
    soon after.  inb on DebugOutputPort doesn't work on Xen, so the check
    will always fail and DebugOutputPort will be cleared to 0 disabling
    output.
    
    Quick exit the function when running on Xen to preserve the modified
    DebugOutputPort.
    
    Signed-off-by: Jason Andryuk <jandryuk@gmail.com>
    Message-Id: <20200617023117.80210-1-jandryuk@gmail.com>

--- src/hw/serialio.c
+++ src/hw/serialio.c
@@ -106,6 +106,10 @@ u16 DebugOutputPort VARFSEG = 0x402;
 void
 qemu_debug_preinit(void)
 {
+    /* Xen already overrode DebugOutputPort in xen_preinit(). */
+    if (runningOnXen())
+        return;
+
     /* Check if the QEMU debug output port is active */
     if (CONFIG_DEBUG_IO &&
         inb(GET_GLOBAL(DebugOutputPort)) != QEMU_DEBUGCON_READBACK)
