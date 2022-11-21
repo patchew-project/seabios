patchew-project/seabios

commit a05af290bac55be65e979e007687d4e27cfa7b8d

virtio-blk: split large IO according to size_max
if driver reads data larger than VIRTIO_BLK_F_SIZE_MAX,
it will cause some issue to the DMA engine.

So when upper software wants to read data larger than
VIRTIO_BLK_F_SIZE_MAX, virtio-blk driver split one large
request into multiple smaller ones.

Signed-off-by: Andy Pei <andy.pei@intel.com>
Signed-off-by: Ding Limin <dinglimin@cmss.chinamobile.com>
Reviewed-by: Gerd Hoffmann <kraxel@redhat.com>

src/hw/virtio-blk.c
@@ -24,6 +24,11 @@
 #include "virtio-ring.h"
 #include "virtio-blk.h"
 
+#define min(a, b) ({\
+		typeof(a) _a = a;\
+		typeof(b) _b = b;\
+		_a < _b ? _a : _b; })
+
 struct virtiodrive_s {
     struct drive_s drive;
     struct vring_virtqueue *vq;
@@ -82,8 +87,36 @@ virtio_blk_op(struct disk_op_s *op, int write)
             .length     = sizeof(status),
         },
     };
+    u32 max_io_size =
+        vdrive->drive.max_segment_size * vdrive->drive.max_segments;
+    u16 blk_num_max;
 
-    virtio_blk_op_one_segment(vdrive, write, sg);
+    if (vdrive->drive.blksize != 0 && max_io_size != 0)
+        blk_num_max = (u16)max_io_size / vdrive->drive.blksize;
+    else
+        /* default blk_num_max if hardware doesnot advise a proper value */
+        blk_num_max = 8;
+
+    if (op->count <= blk_num_max) {
+        virtio_blk_op_one_segment(vdrive, write, sg);
+    } else {
+        void *p  = op->buf_fl;
+        u16 count = op->count;
+
+        while (count > 0) {
+            u16 blk_num = min(count, blk_num_max);
+            sg[1].length = vdrive->drive.blksize * blk_num;
+            sg[1].addr = p;
+            virtio_blk_op_one_segment(vdrive, write, sg);
+            if (status == VIRTIO_BLK_S_OK) {
+                hdr.sector += blk_num;
+                p += sg[1].length;
+                count -= blk_num;
+            } else {
+                break;
+            }
+        }
+    }
     return status == VIRTIO_BLK_S_OK ? DISK_RET_SUCCESS : DISK_RET_EBADTRACK;
 }
 

commit 8f79187f82c48684fea9f0b85b3a0e99719d7909

virtio-blk: Fix incorrect type conversion in virtio_blk_op()
Fixes: a05af290bac5 ("virtio-blk: split large IO according to size_max")

When using spdk aio bdev driver, the qemu command line like this:
qemu-system-x86_64 \
    -chardev socket,id=char0,path=/tmp/vhost.0 \
    -device vhost-user-blk-pci,id=blk0,chardev=char0 \
    ...

Boot failure message as below:

e820 map has 7 items:
  0: 0000000000000000 - 000000000009fc00 = 1 RAM
  1: 000000000009fc00 - 00000000000a0000 = 2 RESERVED
  2: 00000000000f0000 - 0000000000100000 = 2 RESERVED
  3: 0000000000100000 - 000000007ffdd000 = 1 RAM
  4: 000000007ffdd000 - 0000000080000000 = 2 RESERVED
  5: 00000000feffc000 - 00000000ff000000 = 2 RESERVED
  6: 00000000fffc0000 - 0000000100000000 = 2 RESERVED
enter handle_19:
  NULL
Booting from Hard Disk...
Boot failed: could not read the boot disk

Signed-off-by: Xiaofei <hbuxiaofei@gmail.com>
Acked-by: Andy Pei < andy.pei@intel.com>
Acked-by: Gerd Hoffmann <kraxel@redhat.com>
Message-Id: <20221119074750.83531-1-hbuxiaofei@gmail.com>

src/hw/virtio-blk.c
@@ -92,7 +92,7 @@ virtio_blk_op(struct disk_op_s *op, int write)
     u16 blk_num_max;
 
     if (vdrive->drive.blksize != 0 && max_io_size != 0)
-        blk_num_max = (u16)max_io_size / vdrive->drive.blksize;
+        blk_num_max = (u16)(max_io_size / vdrive->drive.blksize);
     else
         /* default blk_num_max if hardware doesnot advise a proper value */
         blk_num_max = 64;